ecc7698eedb593b6387b048ba23ed4f8a236b6aa
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-05-09 13:37:42 +0530

    minor changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-babel-load-languages '((emacs-lisp . t) (python . t) (perl . t) (shell . t)))
+ '(org-babel-load-languages '((emacs-lisp . t) (python . t) (perl . t)))
  '(origami-show-fold-header t)
  '(package-selected-packages
    '(org-bullets undo-fu whitespace-cleanup-mode key-chord evil))
