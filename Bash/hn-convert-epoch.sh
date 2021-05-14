fe0c2c63747a178064dedd8fdae4476e9ece4001
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-05-14 09:34:58 +0530

    convert epoch

---
+++ Bash/hn-convert-epoch.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+# hn-convert-epoch.sh
+#  Convert the trailing number of all arguments from an epoch timestamp to a
+#  human readable timestamp.
+
+
+for i in "$@"; do
+    stamp=$(echo "$i" | grep -oP '\d+$')
+    printf "$i: "
+    date -d @$stamp
+done
