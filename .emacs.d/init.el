8df9fb9432bbae15a0920828a7518273602f52b2
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-05-14 17:48:17 +0530

    minor changes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -72,6 +72,7 @@
 (setq verilog-indent-level-declaration 2)
 (setq verilog-indent-level-module 2)
 (setq verilog-indent-level 2)
+(setq verilog-indent-lists nil)
 (setq verilog-auto-endcomments nil)
 
 ;; General Settings
