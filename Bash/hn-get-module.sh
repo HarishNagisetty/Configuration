3f5de9f30854162ec5449f7e6403599f0079a7ad
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-06-04 17:41:37 +0530

    minor changes

--- Bash/hn-get-module.sh
+++ Bash/hn-get-module.sh
@@ -3,7 +3,7 @@
 # hn-get-module.sh
 #  Get single Verilog module definition from a large netlist
 #
-# Usage: hn-get-module netlist_file "module_name"
+# Usage: hn-get-module netlist_file module_name [output_file]
 
 
 START=$(grep -n -m 1 "module[[:space:]]\+$2" $1 | sed 's/\([0-9]*\).*/\1/')
@@ -14,7 +14,11 @@ if [[ -n "$START" ]]; then
 
     if [[ -n "$LEN" ]]; then
         printf "Saving $LEN lines starting from #$START.\n"
-        tail -n +$START $1 | head -n $LEN > $2.sv.$(date +%s)
+        if [[ -n $3 ]]; then
+            tail -n +$START $1 | head -n $LEN > $2.sv.$(date +%s)
+        else
+            tail -n +$START $1 | head -n $LEN > $3
+        fi
     else
         printf "Could not find \"endmodule\" after line $START\n"
         exit 1
