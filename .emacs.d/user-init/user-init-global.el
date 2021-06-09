49eb3d5f1b7c8026541381190af6fc549c6986bb
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-06-09 22:00:05 +0530

    minor changes

--- .emacs.d/user-init/user-init-global.el
+++ .emacs.d/user-init/user-init-global.el
@@ -1,5 +1,12 @@
 ;;; user-init-global.el --- Global configuration of installed packages
 
+;; dired
 (add-hook 'dired-mode-hook 'hn-no-line-numbers)
+; Evil uses most dired mappings by default.
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (evil-local-set-key 'normal (kbd "w") 'evil-forward-word-begin)
+            (evil-local-set-key 'normal (kbd "gg") 'evil-goto-first-line)
+            (evil-local-set-key 'normal (kbd "G") 'evil-goto-line)))
 
 (provide 'user-init-global)
