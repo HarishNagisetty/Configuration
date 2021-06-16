be897754453decc2de50342fd0c536d21e664f07
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-06-16 10:39:20 +0530

    update emacs config

--- .emacs.d/bindings/bindings-core.el
+++ .emacs.d/bindings/bindings-core.el
@@ -40,20 +40,22 @@
   (when (> (count-windows) 1)
     (delete-window)))
 
+(defun /bindings/core/dired-sidebar-properties ()
+  "Set special properties when using dired like a sidebar."
+  (setq-local /bindings/core/dired-sidebar t)
+  (dired-hide-details-mode)
+  (define-key evil-normal-state-local-map
+    (kbd "<return>") '/bindings/core/open-dired-file))
+
 (defun /bindings/core/open-dired-file ()
   "Open in same window if directory, other window if file"
   (interactive)
   (let ((file (dired-get-file-for-visit))
-        (hide-details (bound-and-true-p dired-hide-details-mode)))
+        (is-sidebar (bound-and-true-p /bindings/core/dired-sidebar)))
     (if (file-directory-p file)
         (progn
           (dired-find-file)
-          (define-key evil-normal-state-local-map
-            (kbd "<return>") '/bindings/core/open-dired-file)
-          ;; If hide-details was previously set, do
-          ;; the same for this buffer too.
-          (when hide-details
-            (dired-hide-details-mode)))
+          (when is-sidebar (/bindings/core/dired-sidebar-properties)))
       (dired-find-file-other-window)
       (other-window -1))))
 
@@ -63,9 +65,7 @@
   (let ((width (round (* 0.33 (window-width)))))
     (split-window-horizontally width)
     (dired default-directory)
-    (dired-hide-details-mode)
-    (define-key evil-normal-state-local-map
-      (kbd "<return>") '/bindings/core/open-dired-file)))
+    (/bindings/core/dired-sidebar-properties)))
 
 (defun /bindings/core/open-marks ()
   (interactive)
@@ -95,99 +95,13 @@
   ("on" #'/bindings/core/open-notes "Open Notes")
   ("os" #'/bindings/core/open-scratch-buffer "Open Scratch Buffer")
   ("q" #'/bindings/core/delete-buffer "Delete Buffer")
+  ("w" nil "Windows...")
+  ("wc" #'evil-window-delete "Delete Window")
+  ("wh" #'evil-window-left "Move Left")
+  ("wj" #'evil-window-down "Move Down")
+  ("wk" #'evil-window-up "Move Up")
+  ("wl" #'evil-window-right "Move Right")
+  ("wo" #'delete-other-windows "Delete Other Windows")
   )
 
 (provide '/bindings/core)
-
-;;;; Markdown Mode
-;(add-hook 'markdown-mode-hook
-;  (lambda ()
-;    (define-key evil-normal-state-local-map
-;      (kbd "<return>")
-;      'hn-follow-markdown-link-on-line)
-;    ))
-;
-;;;; dired
-;;; Show sizes in human readable form
-;;; Don't show user or group
-;(setq dired-listing-switches "-alhgG")
-;(add-hook 'dired-mode-hook 'hn-no-line-numbers)
-;;; Local bindings
-;(eval-after-load 'dired
-;  (lambda ()
-;    ;; Clear local leader
-;    (define-key dired-mode-map (kbd hn-evil-localleader) nil)
-;    (define-key dired-mode-map
-;      (kbd (concat hn-evil-localleader "u")) 'hn-dired-up-directory)
-;    ))
-;;; Add some evil bindings for navigation
-;(add-hook 'dired-mode-hook
-;          (lambda ()
-;            (define-key evil-normal-state-local-map
-;              (kbd "w") 'evil-forward-word-begin)
-;            (define-key evil-normal-state-local-map
-;              (kbd "gg") 'evil-goto-first-line)
-;            (define-key evil-normal-state-local-map
-;              (kbd "G") 'evil-goto-line)))
-;
-;;;; Buffer List
-;;; After selecting buffer through the Buffer List, I don't want evil
-;;; alternate file to be *Buffer List*
-;(advice-add 'Buffer-menu-this-window :after
-;            (lambda () (kill-buffer "*Buffer List*")))
-;
-;
-;;;; Global Key Bindings
-;(eval-after-load 'evil
-;  (lambda ()
-;    ;; Clear leader and local leader for which-key
-;    (define-key evil-motion-state-map (kbd hn-evil-leader) nil)
-;    (define-key evil-motion-state-map (kbd hn-evil-localleader) nil)
-;
-;    (define-key evil-motion-state-map
-;      (kbd "<tab>") 'evil-toggle-fold)
-;
-;    (which-key-add-keymap-based-replacements evil-motion-state-map
-;      (concat hn-evil-leader "o") '("Open...")
-;      (concat hn-evil-leader "om") '("Open Bookmarks")
-;      (concat hn-evil-leader "on") '("Open Notes")
-;      (concat hn-evil-leader "os") '("Open Scratch")
-;      (concat hn-evil-leader "q") '("Delete Buffer")
-;      (concat hn-evil-leader "w") '("Window...")
-;      )
-;
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "ot")) 'hn-dired-open-sidebar)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "om")) (lambda ()
-;                                           (interactive)
-;                                           (split-window-vertically)
-;                                           (find-file "~/.marks")
-;                                           (markdown-mode)))
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "on")) (lambda ()
-;                                           (interactive)
-;                                           (split-window-vertically)
-;                                           (find-file "~/.notes")
-;                                           (hn-set-comment-char "#")))
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "os")) 'hn-open-scratch-buffer)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "q")) (lambda ()
-;                                          (interactive)
-;                                          (kill-this-buffer)
-;                                          (when (> (count-windows) 1)
-;                                            (delete-window))))
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "wc")) 'evil-window-delete)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "wh")) 'evil-window-left)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "wj")) 'evil-window-down)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "wk")) 'evil-window-up)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "wl")) 'evil-window-right)
-;    (define-key evil-motion-state-map
-;      (kbd (concat hn-evil-leader "wo")) 'delete-other-windows)
-;    ))
--- .emacs.d/config/config-util.el
+++ .emacs.d/config/config-util.el
@@ -20,33 +20,11 @@
      (setq org-map-continue-from (org-element-property :begin (org-element-at-point))))
    "/DONE" nil))
 
-(defun /config/util/dired-up-directory ()
-  "Go to parent directory and hide details if previously hidden."
-  (interactive)
-  (let ((hide-details (bound-and-true-p dired-hide-details-mode)))
-    (dired-up-directory)
-    (when hide-details
-      (dired-hide-details-mode))))
-
 (defun /config/util/no-line-numbers ()
   "Disable line numbering for the current buffer."
   (interactive)
   (if (fboundp 'global-display-line-numbers-mode)
       (display-line-numbers-mode 0)
     (linum-mode 0)))
-
-(defun /config/util/follow-markdown-link-on-line ()
-  "Follow the first markdown link on the current line."
-  (interactive)
-  (save-match-data
-    (let ((line (buffer-substring-no-properties
-                 (line-beginning-position)
-                 (line-end-position))))
-      (string-match "\\](\\(.*\\))" line)
-      (let ((path (match-string 1 line)))
-        (if (file-exists-p path)
-            ;(find-file path)
-            (find-file path)
-          (message "Invalid File: %s" path))))))
 
 (provide '/config/util)
