810310d3a7f4f6667e9368ef58942d7255fc238f
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-08-11 14:24:29 +0530

    clean init.sh

--- init.sh
+++ init.sh
@@ -22,69 +22,36 @@ link_file () {
     fi
 }
 
-######
-# ~/ #
-######
-
-link_file "$SCRIPT_DIR"/.bashrc    ~/.bashrc
-touch                              ~/.bashrc.private
-link_file "$SCRIPT_DIR"/.emacs.d   ~/.emacs.d
-link_file "$SCRIPT_DIR"/.inputrc   ~/.inputrc
-link_file "$SCRIPT_DIR"/.tmux.conf ~/.tmux.conf 
-link_file "$SCRIPT_DIR"/.vimrc     ~/.vimrc
-
-##########
-# ~/bin/ #
-##########
-
+link_file "$SCRIPT_DIR"/.bashrc                    ~/.bashrc
+link_file "$SCRIPT_DIR"/.emacs.d                   ~/.emacs.d
+link_file "$SCRIPT_DIR"/.inputrc                   ~/.inputrc
+link_file "$SCRIPT_DIR"/.tmux.conf                 ~/.tmux.conf 
+link_file "$SCRIPT_DIR"/.vimrc                     ~/.vimrc
 link_file "$SCRIPT_DIR"/Bash/hn-convert-epoch.sh   ~/bin/hn-convert-epoch.sh
 link_file "$SCRIPT_DIR"/Bash/status-bar.sh         ~/bin/status-bar.sh
 link_file "$SCRIPT_DIR"/Bash/hn-compress-images.sh ~/bin/hn-compress-images.sh
+link_file "$SCRIPT_DIR"/.config/nvim/init.vim      $XDG_CONFIG_HOME/nvim/init.vim
+link_file "$SCRIPT_DIR"/.config/sway/config        $XDG_CONFIG_HOME/sway/config
 
 # Firefox - Open Private window
 printf "#! /usr/bin/env bash\nfirefox -private-window\n" > ~/bin/firefly
 chmod +x ~/bin/firefly
-
 # Chromium - Open Private window
 printf '#! /usr/bin/env bash\nchromium-browser --incognito --user-data-dir=/tmp/chromium-$(date +%s)\n' > ~/bin/chromium-private
 chmod +x ~/bin/chromium-private
 
-############
-# emacs.d/ #
-############
-
-git clone \
-    "https://github.com/HarishNagisetty/verilog3-mode.git" \
+# .emacs.d/
+git clone "https://github.com/HarishNagisetty/verilog3-mode.git" \
     ".emacs.d/elisp/verilog3-mode/"
-
-git clone \
-    "https://github.com/HarishNagisetty/origami.el.git" \
+git clone "https://github.com/HarishNagisetty/origami.el.git" \
     ".emacs.d/elisp/origami.el/"
-
-###########
-# ~/.vim/ #
-###########
-
-git clone \
-    "https://github.com/HarishNagisetty/vim-colors-solarized.git" \
+# .vim/
+git clone "https://github.com/HarishNagisetty/vim-colors-solarized.git" \
     ~/.vim/pack/vim-colors-solarized/start/vim-colors-solarized/
-
-git clone \
-    "https://github.com/HarishNagisetty/vim-rsi.git" \
+git clone "https://github.com/HarishNagisetty/vim-rsi.git" \
     ~/.vim/pack/vim-rsi/start/vim-rsi/
-
-git clone \
-    "https://github.com/HarishNagisetty/vim-gnupg.git" \
+git clone "https://github.com/HarishNagisetty/vim-gnupg.git" \
     ~/.vim/pack/vim-gnupg/start/vim-gnupg/
-
-git clone \
-    "https://github.com/HarishNagisetty/verilog_systemverilog.vim.git" \
+git clone "https://github.com/HarishNagisetty/verilog_systemverilog.vim.git" \
     ~/.vim/pack/verilog_systemverilog.vim/start/verilog_systemverilog.vim/
-
-####################
-# $XDG_CONFIG_HOME #
-####################
-
-link_file "$SCRIPT_DIR"/.config/nvim/init.vim $XDG_CONFIG_HOME/nvim/init.vim
-link_file "$SCRIPT_DIR"/.config/sway/config   $XDG_CONFIG_HOME/sway/config
 
