439d4a5ac052df5dd5ceb55f8a8b4f76cd999d58
Harish Nagisetty <harish.nagisetty@outlook.com>
2021-11-06 07:57:22 +0530

    add gitconfig, copy_file, clone_repo

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ link_file () {
     local destination=$2
 
     if [[ ! -e "$source" ]]; then
-        echo "$source is not a file!"
+        echo "$source is not present!"
         return 1
     fi
 
@@ -19,6 +19,47 @@ link_file () {
     else
         mkdir -p "$(dirname "$destination")" 
         ln -s "$source" "$destination"
+        echo "---"
+        echo "Created $destination."
+        echo "---"
+    fi
+}
+
+## Usage: copy_file SRC DST
+copy_file () {
+    local source=$1
+    local destination=$2
+
+    if [[ ! -e "$source" ]]; then
+        echo "$source is not present!"
+        return 1
+    fi
+
+    if [[ -e "$destination" ]]; then
+        echo "$destination is already present."
+        return 0
+    else
+        mkdir -p "$(dirname "$destination")" 
+        cp -i "$source" "$destination"
+        echo "---"
+        echo "Created $destination."
+        echo "---"
+    fi
+}
+
+## Usage: clone_repo SRC DST
+clone_repo () {
+    local source=$1
+    local destination=$2
+
+    if [[ -e "$destination" ]]; then
+        echo "$destination is already present."
+        return 0
+    else
+        git clone "$source" "$destination"
+        echo "---"
+        echo "Created $destination."
+        echo "---"
     fi
 }
 
@@ -35,6 +76,7 @@ link_file "$SCRIPT_DIR"/.config/nvim/init.vim      $XDG_CONFIG_HOME/nvim/init.vi
 link_file "$SCRIPT_DIR"/.config/sway/config        $XDG_CONFIG_HOME/sway/config
 link_file "$SCRIPT_DIR"/.config/alacritty/alacritty.yml \
           $XDG_CONFIG_HOME/alacritty/alacritty.yml
+copy_file "$SCRIPT_DIR"/.gitconfig.global          ~/.gitconfig
 
 # Firefox - Open Private window
 printf "#! /usr/bin/env bash\nfirefox -private-window\n" > ~/bin/firefly
@@ -44,17 +86,17 @@ printf '#! /usr/bin/env bash\nchromium-browser --incognito --user-data-dir=/tmp/
 chmod +x ~/bin/chromium-private
 
 # .emacs.d/
-git clone "https://github.com/HarishNagisetty/verilog3-mode.git" \
+clone_repo "https://github.com/HarishNagisetty/verilog3-mode.git" \
     ".emacs.d/elisp/verilog3-mode/"
-git clone "https://github.com/HarishNagisetty/origami.el.git" \
+clone_repo "https://github.com/HarishNagisetty/origami.el.git" \
     ".emacs.d/elisp/origami.el/"
 # .vim/
-git clone "https://github.com/HarishNagisetty/vim-colors-solarized.git" \
+clone_repo "https://github.com/HarishNagisetty/vim-colors-solarized.git" \
     ~/.vim/pack/vim-colors-solarized/start/vim-colors-solarized/
-git clone "https://github.com/HarishNagisetty/vim-rsi.git" \
+clone_repo "https://github.com/HarishNagisetty/vim-rsi.git" \
     ~/.vim/pack/vim-rsi/start/vim-rsi/
-git clone "https://github.com/HarishNagisetty/vim-gnupg.git" \
+clone_repo "https://github.com/HarishNagisetty/vim-gnupg.git" \
     ~/.vim/pack/vim-gnupg/start/vim-gnupg/
-git clone "https://github.com/HarishNagisetty/verilog_systemverilog.vim.git" \
+clone_repo "https://github.com/HarishNagisetty/verilog_systemverilog.vim.git" \
     ~/.vim/pack/verilog_systemverilog.vim/start/verilog_systemverilog.vim/
 
