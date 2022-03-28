5fb998e831e902573927773435e7c8fd2eec1417
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-03-28 10:34:45 +0530

    add chromium-freeworld

--- Bash/hn-chromium.sh
+++ Bash/hn-chromium.sh
@@ -2,6 +2,12 @@
 
 DATA_DIR=$(mktemp -p /tmp -d chromium-data.XXXXXX.d)
 
-chromium-browser --user-data-dir="$DATA_DIR" #--incognito
+if command -v chromium-freeworld; then
+    EXE=chromium-freeworld
+else
+    EXE=chromium-browser
+fi
+
+$EXE --user-data-dir="$DATA_DIR" #--incognito
 
 rm -rf "$DATA_DIR"
