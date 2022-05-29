7bda3dbd000b874b40bb9f81ea68c2014ba59a03
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-05-29 21:13:43 +0530

    add M-ret binding for occur

--- .emacs.d/init/bindings/occur.el
+++ .emacs.d/init/bindings/occur.el
@@ -6,4 +6,5 @@
 ;; Press <return> to goto occurrence.
 (evil-define-key 'motion occur-mode-map
   [mouse-1] #'occur-mode-goto-occurrence
-  (kbd "<return>") #'occur-mode-display-occurrence)
+  (kbd "<return>") #'occur-mode-display-occurrence
+  (kbd "M-<return>") #'occur-mode-goto-occurrence)
