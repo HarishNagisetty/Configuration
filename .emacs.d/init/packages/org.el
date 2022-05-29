3beb470e75cb8a630f6200c862de15293ceb7dd3
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-05-29 10:58:49 +0530

    add git cm and org todo CANCELLED

--- .emacs.d/init/packages/org.el
+++ .emacs.d/init/packages/org.el
@@ -2,7 +2,7 @@
 
 (setq org-startup-folded t)
 (setq org-todo-keywords
-      '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELLED")))
 (setq org-log-done 'time)
 (setq org-cycle-open-archived-trees nil)
 (setq org-archive-location "~/Cloud/org/archive.org::* From %s")
