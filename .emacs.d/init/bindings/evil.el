8e2aaf63609460eb58bfaf3373dab567729a1a4b
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-05-29 15:48:34 +0530

    toggle fold with tab in emacs

--- .emacs.d/init/bindings/evil.el
+++ .emacs.d/init/bindings/evil.el
@@ -4,5 +4,8 @@
 (setq evil-insert-state-map (make-sparse-keymap))
 (define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
 
+;; Use tab to toggle folding.
+(define-key evil-normal-state-map (kbd "<tab>") 'evil-toggle-fold)
+
 ;; jk to return to normal state.
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
