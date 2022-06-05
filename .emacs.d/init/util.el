c7bc48e90d3cd99cc12590c807aca67376769f23
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-06-05 16:42:33 +0530

    add rename-file-and-buffer

--- .emacs.d/init/util.el
+++ .emacs.d/init/util.el
@@ -99,3 +99,19 @@
     (when active
       (occur (cdr active))
       (other-window 1))))
+
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun /init/util/rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file filename new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
