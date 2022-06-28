fa894b72f0742946db4f3cc02d1dd7a73074b8e6
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-06-28 15:04:50 +0530

    simplify personal theme

--- .emacs.d/themes/personal-light-theme.el
+++ .emacs.d/themes/personal-light-theme.el
@@ -37,38 +37,13 @@
          :background ,inactive-color
          :box (:line-width 3 :color ,inactive-color :style nil))))
 
-   ;; Font-lock
-   `(font-lock-comment-face ((t :foreground ,comment-fg-color
-                                :background ,comment-bg-color
-                                :weight bold)))
-   `(font-lock-function-name-face ((t :foreground ,function-color
-                                      :weight bold)))
-
    ;; Outline
-   `(outline-1 ((t :foreground ,blue-color  :height 1.3)))
-   `(outline-2 ((t :foreground ,green-color :height 1.2)))
-   `(outline-3 ((t :foreground ,red-color   :height 1.1)))
-   `(outline-4 ((t :foreground ,brown-color :height 1.0)))
-   `(outline-5 ((t :foreground ,blue-color  :height 1.0)))
-   `(outline-6 ((t :foreground ,green-color :height 1.0)))
-   `(outline-7 ((t :foreground ,red-color   :height 1.0)))
-   `(outline-8 ((t :foreground ,brown-color :height 1.0)))
+   `(outline-1 ((t :inherit font-lock-function-name-face :height 1.3)))
+   `(outline-2 ((t :inherit font-lock-variable-name-face :height 1.2)))
+   `(outline-3 ((t :inherit font-lock-keyword-face :height 1.1)))
 
    ;; Whitespace
    `(whitespace-trailing ((t :background ,trail-ws-color)))
-
-   ;; Org
-   `(org-document-title ((t :foreground ,salient-color :height 1.1)))
-   `(org-done           ((t :foreground ,faded-color :strike-through t)))
-   `(org-level-1        ((t :inherit outline-1)))
-   `(org-level-2        ((t :inherit outline-2)))
-   `(org-level-3        ((t :inherit outline-3)))
-   `(org-level-4        ((t :inherit outline-4)))
-   `(org-level-5        ((t :inherit outline-5)))
-   `(org-level-6        ((t :inherit outline-6)))
-   `(org-level-7        ((t :inherit outline-7)))
-   `(org-level-8        ((t :inherit outline-8)))
-   `(org-headline-done  ((t :foreground ,faded-color)))
    ))
 
 (provide-theme 'personal-light)
