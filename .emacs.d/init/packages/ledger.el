fc4961563e1a4395bdc0533a57514e5ecf854f8a
Harish Nagisetty <harish.nagisetty@outlook.com>
2022-06-28 12:17:54 +0530

    ledger disable highlight xact

--- .emacs.d/init/packages/ledger.el
+++ .emacs.d/init/packages/ledger.el
@@ -2,5 +2,6 @@
 
 (setq-default ledger-post-amount-alignment-column 58)
 (setq-default ledger-post-account-alignment-column 2)
+(setq-default ledger-highlight-xact-under-point nil)
 
 (/init/util/require-package 'ledger-mode)
